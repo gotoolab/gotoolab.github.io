beaa3fe96b21268511b930e1f74cbee3ba6d1206
nishuvo <nishuvo@outlook.com>
2019-05-04 06:23:19 +0600

    contact form fixes 1.1

--- js/gl.js
+++ js/gl.js
@@ -19,41 +19,41 @@ $('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
 
-// contact form validator 
+// contact form actions (require jQuery validator)
 $("#contactform").validate({
     rules: {
-        name: {
-            required: true
-        },
-        email: {
-            required: true,
-            email: true
-        }
+      name: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      }
     },
-    submitHandler: function (form) {
-        var form = $("#contactform");
-        var submitButton = $("#submit");
-        var response = $('#response');
-        $.ajax({
-            method: "POST",
-            url: "//formspree.io/nishuvo@gotoolab.com",
-            data: $("#contactform").serialize(),
-            dataType: "json",
-
-            beforeSend: function() {
-                response.text('Sending....');
-              },
-              success: function(data) {
-                response.html('<i class="fas fa-check"></i> Message sent').fadeIn();
-                form.trigger('reset');
-              },
-              error: function(e) {
-                console.log(e);
-                response.text('There was an error!');
-            }
-        });
+    submitHandler: function(form) {
+      var form = $("#contactform");
+      var submitButton = $("#submit");
+      var response = $('#response');
+      $.ajax({
+        method: "POST",
+        url: "//formspree.io/nishuvo@gotoolab.com",
+        data: $("#contactform").serialize(),
+        dataType: "json",
+  
+        beforeSend: function() {
+          response.text('Sending....');
+        },
+        success: function(data) {
+          response.html('<i class="fas fa-check"></i> Message sent').fadeIn();
+          form.trigger('reset');
+        },
+        error: function(e) {
+          console.log(e);
+          response.text('There was an error!');
+        }
+      });
     }
-});
+  });
 
 // get the year
 document.getElementById("year").innerHTML = new Date().getFullYear();
